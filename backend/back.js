3a08d2d8a1b2e8837d8045b8857a722ad74fd18c
Olaoluwa <olaoluwalawal011@gmail.com>
2026-05-26 04:35:04 -0700

    added cors

--- backend/back.js
+++ backend/back.js
@@ -11,7 +11,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://react-project-org.vercel.app/"
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
